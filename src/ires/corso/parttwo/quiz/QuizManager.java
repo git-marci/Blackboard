29a23ffc29413ca4f6db2c725cb5121364e6416c
git-marci <marcello_mauro@hotmail.com>
2021-06-25 14:26:08 +0200

    Corretto layout delle prime righe della class definition (prova per git merge)

--- src/ires/corso/parttwo/quiz/QuizManager.java
+++ src/ires/corso/parttwo/quiz/QuizManager.java
@@ -3,12 +3,13 @@ package ires.corso.parttwo.quiz;
 import java.io.IOException;
 import java.util.Iterator;
 
-public class QuizManager {
-
-    public static void main(String[] args) throws IOException {
-      //1 Chiede all'utente l'autenticazione
-      //2 Chiede all'utente quale quiz vuole fare
-      //3 Riceve risposta utente e istanzia il quiz corretto
+public class QuizManager
+{
+    public static void main(String[] args) throws IOException
+    {
+        //1 Chiede all'utente l'autenticazione
+        //2 Chiede all'utente quale quiz vuole fare
+        //3 Riceve risposta utente e istanzia il quiz corretto
         //4 Loop del quiz
         //5 Mostra il punteggio
         //6 Chiede se uscire o farne un altro
