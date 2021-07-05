55bdd0bc1e42690cc4adc34fd4d6e768c421a105
git-marci <marcello_mauro@hotmail.com>
2021-07-05 17:12:27 +0200

    Esercizio repository "enhanced"

---
+++ src/ires/corso/partfour/BB210705.java
@@ -0,0 +1,20 @@
+package ires.corso.partfour;
+
+public class BB210705
+{
+    // Date:
+    // -----
+    // - una classe Vehicle
+    // - tre classi Car, Truck, Motorbike (mantenetele + scarne possibile) che la estendono
+    // - organizzare un repository "polimorfico", che gestisca le tre tipologie di veicolo
+    //   utilizzando un'unica collezione (di Vehicle)
+    // - prevedere almeno le seguenti funzionalità:
+    //
+    //   1) restituire lista di veicoli di un solo tipo
+    //   2) restituire lista totale
+    //   3) aggiungere/rimuovere/sostituire (swap) un qualsiasi veicolo
+    //   4) serializzazione... writeObject
+    //
+    // - suggerimento (ma non obbligatorio): ci sono anche i METODI generic...
+    // - opzionale: rendere "generic" il repository
+}
